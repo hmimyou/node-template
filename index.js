e226ad716cefe3041ba500fed4a41581d03987dd
Jingchao Chen <jcc8887@gmail.com>
2017-10-11 23:42:42 -0700

    restructure server

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const express = require('express');
-const app = express();
-const fetchConfig = require('zero-config');
-const format = require('string-format');
-format.extend(String.prototype);
-
-const config = fetchConfig(__dirname);
-const port = config.get('app.port');
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(port, function() {
-  console.log('Example app listening on port {0}!'.format(port));
-});
+// TODO: change to dist-server
+require('./src/server/app.js').startServer();
---
+++ src/server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fetchConfig = require('zero-config');
+const format = require('string-format');
+format.extend(String.prototype);
+
+const config = fetchConfig('{0}/../../'.format(__dirname));
+const port = config.get('server.port');
+
+function startServer(callback) {
+  app.get('/', function(req, res) {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, function() {
+    console.log('Example app listening on port {0}!'.format(port));
+
+    if (typeof callback === 'function') {
+      return callback(app);
+    }
+    return null;
+  });
+}
+
+exports.startServer = startServer;
