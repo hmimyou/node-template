1cdac272a7dcf864deec4bf367f9d07b89f79b87
Jingchao Chen <jcc8887@gmail.com>
2017-10-11 23:18:38 -0700

    add prettier

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const fetchConfig = require('zero-config');
 const format = require('string-format');
-format.extend(String.prototype)
+format.extend(String.prototype);
 
 const config = fetchConfig(__dirname);
 const port = config.get('app.port');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World!');
-})
+});
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Example app listening on port {0}!'.format(port));
 });
